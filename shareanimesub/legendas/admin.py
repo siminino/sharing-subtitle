18e9578c62f25b58fc53b56ca6f6cccc0f0aaec8
ubuntu <siminino@ubuntu.(none)>
2012-03-04 19:35:10 -0300

    adicionando filter

--- shareanimesub/legendas/admin.py
+++ shareanimesub/legendas/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from legendas.models import Legenda
 
-admin.site.register(Legenda)
+
+class LegendaAdmin(admin.ModelAdmin):
+    list_filter = ('projeto',)
+
+admin.site.register(Legenda, LegendaAdmin)
